fad1b6d3fd956df91c93c03a6d5968dd20c06ce2
Michelle Bu <michelle@michellebu.com>
2013-04-13 16:49:10 -0700

    misc

--- src/xhr_handler.erl
+++ src/xhr_handler.erl
@@ -1,16 +1,27 @@
 %% @doc PeerJS XHR handler.
 
 -module(xhr_handler).
--behaviour(cowboy_http_handler).
+-behaviour(cowboy_loop_handler).
 
 -export([init/3]).
--export([handle/2]).
+-export([info/3]).
 -export([terminate/3]).
 
-init({tcp, http}, Req, Opts) ->
-    {ok, Req, undefined_state}.
+-record(state, { key, token, id, ip, action }).
 
-handle(Req, State) ->
+init({tcp, http}, Req, Opts) ->
+    {ok, Req2} = cowboy_req:chunked_reply(200,
+        [{<<"content-type">>, <<"application/octet-stream">>}],
+        Req),
+    ok = cowboy_req:chunk(util:long_string()),
+    State = #state{ key = cowboy_req:binding(key, Req),
+                    req = Req2,
+                    token = cowboy_req:binding(token, Req),
+                    id = cowboy_req:binding(id, Req),
+                    action = cowboy_req:binding(action, Req) },
+    {loop, Req, State}.
+
+info(Message, Req, State) ->
     {ok, Req2} = cowboy_req:reply(200, [], <<"Hello World!">>, Req),
     {ok, Req2, State}.
 
