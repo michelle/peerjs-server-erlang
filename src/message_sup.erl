b414cccb7d3cb245b38e9a94b85163dcb864675e
Michelle Bu <michelle@michellebu.com>
2013-04-13 19:20:21 -0700

    src

--- src/message_sup.erl
+++ src/message_sup.erl
@@ -1,5 +1,5 @@
 %% @private
--module(websocket_sup).
+-module(message_sup).
 -behaviour(supervisor).
 
 %% API.
@@ -9,13 +9,11 @@
 -export([init/1]).
 
 %% API.
-
 -spec start_link() -> {ok, pid()}.
 start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %% supervisor.
-
 init([]) ->
   Procs = [],
   {ok, {{one_for_one, 10, 10}, Procs}}.
